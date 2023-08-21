a529cb9744314d7f8aa34ce640089e5a2d7c4b61
Anthony Zhang <97823062+anthonyjzhang@users.noreply.github.com>
2023-08-21 05:21:41 -0400

    Update About.jsx

--- src/pages/home/About.jsx
+++ src/pages/home/About.jsx
@@ -77,7 +77,7 @@ const About = () => {
               <img
                 src="/work4.png"
                 alt=""
-                className="object-contain h-[80px] sm:mt-0 mt-6"
+                className="object-contain h-[65px] sm:mt-0 mt-6"
               />
               <img
                 src="/work5.png"
