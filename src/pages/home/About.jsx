1698160700adc6eab75fcd91c231f1a684c89e86
Anthony Zhang <97823062+anthonyjzhang@users.noreply.github.com>
2023-08-21 05:17:12 -0400

    Update About.jsx

--- src/pages/home/About.jsx
+++ src/pages/home/About.jsx
@@ -60,24 +60,24 @@ const About = () => {
           <div className="grid grid-cols-2 sm:grid-cols-3 gap-y-3 gap-x-6 w-full max-w-[1000px] place-items-center">
             <Fade damping={0.1} cascade direction="up">
               <img
-                className="object-contain h-[150px]"
+                className="object-contain h-[130px]"
                 src="/work1.png"
                 alt=""
               />
               <img
                 src="/work2.png"
                 alt=""
-                className="object-contain h-[150px]"
+                className="object-contain h-[130px]"
               />
               <img
                 src="/work3.png"
                 alt=""
-                className="object-contain sm:mt-0 mt-6 h-[160px]"
+                className="object-contain sm:mt-0 mt-6 h-[140px]"
               />
               <img
                 src="/work4.png"
                 alt=""
-                className="object-contain h-[85px] sm:mt-0 mt-6"
+                className="object-contain h-[75px] sm:mt-0 mt-6"
               />
               <img
                 src="/work5.png"
