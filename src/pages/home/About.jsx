774b217c94526e77b547da945d00e3249de647e0
Anthony Zhang <anthony.zhang@duke.edu>
2023-12-06 05:47:49 -0500

    updates

--- src/pages/home/About.jsx
+++ src/pages/home/About.jsx
@@ -82,7 +82,7 @@ const About = () => {
               <img
                 src="/work5.png"
                 alt=""
-                className="object-contain sm:-mt-2 h-[81px]"
+                className="object-contain sm:-mt-2 h-[70px]"
               />
               <img
                 src="/work6.png"
